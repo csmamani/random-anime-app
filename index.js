f551eb6cd19e649fb25f2705d602965bf7edb0ea
Carolin <mamaniccarolina@gmail.com>
2020-06-13 21:07:52 -0300

    Starting to use Jikan API to get animes

---
+++ index.js
@@ -0,0 +1,20 @@
+const API_URL = 'https://api.jikan.moe/v3/anime/';
+
+const randomAnimeCard = document.getElementById('anime-card');
+const button = document.getElementById('btn');
+
+const randomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const getRandomAnime = () => {
+
+    randomAnimeCard.innerHTML = '';
+
+    fetch(`${API_URL}${randomNumber(1, 5114)}`)
+        .then(res => res.json())
+        .then(data => {
+            randomAnimeCard.innerHTML = `<img src="${data.image_url}"
+            <p>You got ${data.title}!</p>`;
+        });
+}
+
+button.addEventListener("click", getRandomAnime);
