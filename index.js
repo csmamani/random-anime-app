70f4d7e39b8cd002e3a9763dce871d7751f10a13
Carolin <mamaniccarolina@gmail.com>
2020-08-03 22:22:00 -0300

    Fixing a bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const API_URL = "https://api.jikan.moe/v3/anime/";
 
-const randomAnimeCard = document.getElementById("anime-card");
+const randomAnimeCard = document.getElementById("anime-card-section");
 const button = document.getElementById("btn");
 
 const randomNumber = (min, max) =>
@@ -12,8 +12,8 @@ const getRandomAnime = () => {
   fetch(`${API_URL}${randomNumber(1, 5114)}`)
     .then((res) => res.json())
     .then((data) => {
-      randomAnimeCard.innerHTML = `<img class="anime-image" src="${data.image_url}">
-            <p class="anime-title">You got ${data.title}!</p>`;
+      randomAnimeCard.innerHTML = `<div class="anime-card" id="anime-card"><img class="anime-image" src="${data.image_url}">
+            <p class="anime-title">You got ${data.title}!</p></div>`;
     });
 };
 
